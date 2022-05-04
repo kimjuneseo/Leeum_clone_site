9d048ed76f47eb6c7e6974477d81178a070a61ed
kimjuneseo <june3929213512@gmail.com>
2022-05-04 14:15:21 +0900

    sub2도 완성

--- js/script.js
+++ js/script.js
@@ -1,63 +1,14 @@
-const $ = (el, one= true) => one ? document.querySelector(el) :document.querySelectorAll(el);
+const $ = (el, one= true) => one ? document?.querySelector(el) :document?.querySelectorAll(el);
 
 
-window.onload = (e) => {
-    op("header .menuBtn");
-    op("header .menu", 100);
-    op("header .logo", 200);
-    op("#slide .text_box", 300);
-
-}
-
-const op = (el, delay) => {
-    setTimeout(() => {
-        $(el).classList.add("op");
-    }, delay);
-};
-
-
-window.onwheel = (e) => {
+[...$('.boro a', false)].map(a => {
     
-    console.log(e)
-}
+    a.addEventListener("click", e => {
+        [...$('.boro a', false)].map(el => el.classList.remove('cici'))
+        a.classList.add('cici');
 
-let swiper = new Swiper(".mySwiper", {
-    slidesPerView: 2,
-    spaceBetween: 30,
-    slidesPerGroup: 1,
-    loop: true,
-    loopFillGroupWithBlank: true,
-    autoplay: {
-        delay: 2000,
-    },
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true
-    },
-    navigation: {
-        nextEl: ".next1",
-        prevEl: ".prev1"
-    },
+    });
+}) 
 
-});
 
-let swiper2 = new Swiper(".mySwiper2", {
-    slidesPerView: 2,
-    spaceBetween: 30,
-    slidesPerGroup: 1,
-    loop: true,
-    loopFillGroupWithBlank: true,
-    autoplay: {
-        delay: 2000,
-    },
-    pagination: {
-        el: ".swiper-pagination2",
-        clickable: true
-    },
-    navigation: {
-        nextEl: ".next2",
-        prevEl: ".prev2"
-    },
-
-});
 
